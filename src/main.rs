d2889a576e5d94ed0ec3fe7bc7afa232f3218702
RC James Harlow <git@james.harlow.name>
2021-05-08 16:41:55 +0100

    construct and print a nn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use ndarray::{arr1, Array1};
+use ndarray_rand::RandomExt;
+use ndarray_rand::rand_distr::Uniform;
+
+#[derive(Debug)]
+struct NeuralNetwork {
+    input: Array1<f64>,
+    weights1: Array1<f64>,
+    weights2: Array1<f64>,
+    y: Array1<f64>,
+    output: Array1<f64>
+}
+
+impl NeuralNetwork {
+    pub fn new(x: &Array1<f64>, y: &Array1<f64>) -> NeuralNetwork {
+        NeuralNetwork{
+            input: x.clone(),
+            weights1: Array1::random(4, Uniform::new(0., 1.)),
+            weights2: Array1::random(4, Uniform::new(0., 1.)),
+            y: y.clone(),
+            output: Array1::zeros(y.len())
+
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", NeuralNetwork::new(&arr1(&[1., 2., 3., 4.]), &arr1(&[1., 0., 0., 1.])));
 }
