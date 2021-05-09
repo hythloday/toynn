ebcee22172c065d1a21b2804ea06a7507aeead81
RC James Harlow <git@james.harlow.name>
2021-05-09 19:37:39 +0100

    backprop & train

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use ndarray::{arr2, Array2};
+use ndarray::{azip, arr2, Array2};
 use ndarray_rand::RandomExt;
 use ndarray_rand::rand_distr::Uniform;
 
@@ -18,7 +18,14 @@ pub fn sigmoid(z: &f64) -> f64 {
     1. / (1. + E.powf(-z))
 }
 
+// derivative of the sigmoid function
+pub fn d_sigmoid(z: &f64) -> f64 {
+    use std::f64::consts::E;
+    E.powf(-z) / (1. + E.powf(-z)).powf(2.)
+}
+
 impl NeuralNetwork {
+
     pub fn new(x: &Array2<f64>, y: &Array2<f64>) -> NeuralNetwork {
         NeuralNetwork{
             input: x.clone(),
@@ -34,6 +41,18 @@ impl NeuralNetwork {
         self.layer1 = self.input.dot(&self.weights1).map(sigmoid);
         self.output = self.layer1.dot(&self.weights2).map(sigmoid);
     }
+
+    /*
+        loss = 2 * (y-output) * d_sig(output)
+        weights1 += self.input.T . ((loss . self.weights2.T) * d_sig(layer1))
+        weights2 += = self.layer1.T . loss
+    */
+    pub fn bp(self: &mut Self) {
+        let mut loss = Array2::zeros(self.y.dim()); azip!((l in &mut loss, y in &self.y, o in &self.output) *l = 2. * (y - o) * d_sigmoid(o));
+        azip!((w in &mut self.weights2, d in &self.layer1.t().dot(&loss)) *w += d);
+        let mut chain = Array2::zeros(self.layer1.dim()); azip!((c in &mut chain, a in &loss.dot(&self.weights2.t()), b in &self.layer1) *c = a * d_sigmoid(b));
+        azip!((w in &mut self.weights1, d in &self.input.t().dot(&chain)) *w += d);
+    }
 }
 
 fn main() {
@@ -42,10 +61,12 @@ fn main() {
         [0., 1., 1.],
         [1., 0., 1.],
         [1., 1., 1.]
-    ]).t().into_owned();
-    let y = arr2(&[[1.], [0.], [0.], [1.]]);
+    ]);
+    let y = arr2(&[[0.], [1.], [1.], [0.]]);
     let mut nn = NeuralNetwork::new(&input, &y);
-    nn.ff();
-    println!("{:?}", nn.layer1);
-
+    for _ in 0..1500 {
+        nn.ff();
+        nn.bp();
+    }
+    println!("{:?}", nn.output);
 }
