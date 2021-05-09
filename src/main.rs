5b2a1bfe84da00ed49eb9ed9556b2a802d48a7a5
RC James Harlow <git@james.harlow.name>
2021-05-09 19:40:29 +0100

    rustsfmt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use ndarray::{azip, arr2, Array2};
-use ndarray_rand::RandomExt;
+use ndarray::{arr2, azip, Array2};
 use ndarray_rand::rand_distr::Uniform;
+use ndarray_rand::RandomExt;
 
 #[derive(Debug)]
 struct NeuralNetwork {
@@ -9,7 +9,7 @@ struct NeuralNetwork {
     layer1: Array2<f64>,
     weights2: Array2<f64>,
     y: Array2<f64>,
-    output: Array2<f64>
+    output: Array2<f64>,
 }
 
 // sigmoid function.
@@ -25,15 +25,14 @@ pub fn d_sigmoid(z: &f64) -> f64 {
 }
 
 impl NeuralNetwork {
-
     pub fn new(x: &Array2<f64>, y: &Array2<f64>) -> NeuralNetwork {
-        NeuralNetwork{
+        NeuralNetwork {
             input: x.clone(),
             weights1: Array2::random((x.dim().1, 4), Uniform::new(0., 1.)),
             layer1: Array2::zeros(y.dim()),
             weights2: Array2::random((4, 1), Uniform::new(0., 1.)),
             y: y.clone(),
-            output: Array2::zeros(y.dim())
+            output: Array2::zeros(y.dim()),
         }
     }
 
@@ -48,14 +47,17 @@ impl NeuralNetwork {
         weights2 += = self.layer1.T . loss
     */
     pub fn bp(self: &mut Self) {
-        let mut loss = Array2::zeros(self.y.dim()); azip!((l in &mut loss, y in &self.y, o in &self.output) *l = 2. * (y - o) * d_sigmoid(o));
+        let mut loss = Array2::zeros(self.y.dim());
+        azip!((l in &mut loss, y in &self.y, o in &self.output) *l = 2. * (y - o) * d_sigmoid(o));
         azip!((w in &mut self.weights2, d in &self.layer1.t().dot(&loss)) *w += d);
-        let mut chain = Array2::zeros(self.layer1.dim()); azip!((c in &mut chain, a in &loss.dot(&self.weights2.t()), b in &self.layer1) *c = a * d_sigmoid(b));
+        let mut chain = Array2::zeros(self.layer1.dim());
+        azip!((c in &mut chain, a in &loss.dot(&self.weights2.t()), b in &self.layer1) *c = a * d_sigmoid(b));
         azip!((w in &mut self.weights1, d in &self.input.t().dot(&chain)) *w += d);
     }
 }
 
 fn main() {
+    #[rustfmt::skip]
     let input = arr2(&[
         [0., 0., 1.],
         [0., 1., 1.],
